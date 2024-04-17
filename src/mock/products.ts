d64d35fd06dd080ee2820384f07bc16dd4c32f3f
luizmantuano <luguto23@hotmail.com>
2024-04-17 20:20:47 -0300

    feat: 'Update'

---
+++ src/mock/products.ts
@@ -0,0 +1,64 @@
+export const ProductMockApi = {
+  products: [
+    {
+      title: 'Cardápio de bebidas',
+      productId: 1,
+      productsList: [
+        {
+          productId: 1,
+          img: 'https://images.pexels.com/photos/1672304/pexels-photo-1672304.jpeg?auto=compress&cs=tinysrgb&w=600',
+          name: 'Choop',
+          price: '10,90',
+        },
+        {
+          productId: 2,
+          img: 'https://images.pexels.com/photos/4068383/pexels-photo-4068383.jpeg?auto=compress&cs=tinysrgb&w=600',
+          name: 'Corona',
+          price: '9,50',
+        },
+        {
+          productId: 3,
+          img: 'https://images.pexels.com/photos/17409432/pexels-photo-17409432/free-photo-of-cerveja-orvalho-rocio-marca.jpeg?auto=compress&cs=tinysrgb&w=600',
+          name: 'Brahma',
+          price: '10,90',
+        },
+        {
+          productId: 4,
+          img: 'https://images.pexels.com/photos/13123570/pexels-photo-13123570.png?auto=compress&cs=tinysrgb&w=600',
+          name: 'Jack Deniels',
+          price: '150,90',
+        },
+      ],
+    },
+    {
+      title: 'Cardápio de Comidas',
+      productId: 2,
+      productsList: [
+        {
+          productId: 1,
+          img: 'https://media.istockphoto.com/id/491517422/pt/foto/fuba-bolo.jpg?b=1&s=612x612&w=0&k=20&c=LxEpYJCtUa3-qNWZ7kt5GOlxoo07g6Wd4gMDJlLwUPg=',
+          name: 'Bolo',
+          price: '25,50',
+        },
+        {
+          productId: 2,
+          img: 'https://images.pexels.com/photos/4110226/pexels-photo-4110226.jpeg?auto=compress&cs=tinysrgb&w=600',
+          name: 'Ovos',
+          price: '20,00',
+        },
+        {
+          productId: 3,
+          img: 'https://media.istockphoto.com/id/1165114438/pt/foto/boiled-rice-in-a-bowl.jpg?b=1&s=612x612&w=0&k=20&c=eB-KSx_zO9URtYM3k0DAJXfx0IwiFF9nIglJyJPHNwI=',
+          name: 'Arroz',
+          price: '29,50',
+        },
+        {
+          productId: 4,
+          img: 'https://media.istockphoto.com/id/1361420112/pt/foto/carioca-bean-texture-background.jpg?b=1&s=612x612&w=0&k=20&c=9tfrkdpHi-Uxs3GwEhue07Otd__bs9x8AAPkt07bzvI=',
+          name: 'Feijão',
+          price: '15,50',
+        },
+      ],
+    },
+  ],
+}
