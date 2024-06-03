f6f7b68cd74f57e212d4e370266b165fb88ea715
luizmantuano <luguto23@hotmail.com>
2024-06-03 16:39:45 -0300

    feat: 'Smooth scrolling was added as soon as you click on the topics on the page'

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -17,48 +17,61 @@ export default function Menu(props: ProductsListParams) {
   const [selectedProduct, setSelectedProduct] = useState<number | null>(null)
 
   const toggleProductVisibility = (productId: number) => {
-    setSelectedProduct(productId === selectedProduct ? null : productId)
+    setSelectedProduct(productId)
   }
 
   return (
     <div
-      className="bg-bg-bannerTwo bg-center bg-cover h-[800px] mt-[90px] "
+      className="bg-bg-bannerTwo bg-center bg-cover h-[800px] mt-[90px]"
       id="menu"
     >
-      <div className="flex items-center justify-center ">
+      <div className="flex items-center justify-center">
         <ul className="w-full md:flex md:justify-center md:ml-[350px]">
           {props.data?.map((product) => (
-            <li key={product.productId}>
-              <div className="grid items-center justify-center mt-16 ">
+            <li key={product.productId} className="mt-16">
+              <div className="flex items-center justify-center">
                 <button
                   onClick={() => toggleProductVisibility(product.productId)}
-                  className={`w-[200px] text-white hover:border-[#CC0D03] hover:border-b hover:inline-block  ${selectedProduct === product.productId ? 'border-b border-[#CC0D03]' : ''}`}
+                  className={`w-[200px] text-white hover:border-[#CC0D03] hover:border-b hover:inline-block md:flex md:justify-center ${
+                    selectedProduct === product.productId
+                      ? 'border-b border-[#CC0D03]'
+                      : ''
+                  }`}
                 >
                   {product.title}
                 </button>
               </div>
-              {selectedProduct === product.productId && (
-                <ul className="space-y-[40px] mt-[20px] overflow-auto scrollbar-hide ">
-                  {product.productsList.map((productList) => (
-                    <li key={productList.productId}>
-                      <div className="flex flex-row justify-around items-center ">
-                        <img
-                          src={productList.img}
-                          alt="#"
-                          className="w-[40px] h-[40px] rounded-full object-cover"
-                        />
-                        <h1 className="text-white">{productList.name}</h1>
-                        <span className="text-[#FFD200]">
-                          {formatPrice(productList.price)}
-                        </span>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              )}
             </li>
           ))}
         </ul>
+      </div>
+      <div className="flex items-center justify-center mt-8">
+        {props.data?.map((product) => (
+          <div
+            key={product.productId}
+            className={`${
+              selectedProduct === product.productId ? 'block' : 'hidden'
+            } w-full md:w-auto md:ml-[350px]`}
+          >
+            <ul className="space-y-[40px] mt-[20px] overflow-auto scrollbar-hide">
+              {product.productsList.map((productList) => (
+                <li key={productList.productId}>
+                  <div className="flex flex-row justify-around items-center md:space-x-[100px]">
+                    <img
+                      src={productList.img}
+                      alt="#"
+                      className="w-[40px] h-[40px] rounded-full object-cover"
+                    />
+                    <h1 className="text-white">{productList.name}</h1>
+                    <span className="text-[#FFD200]">
+                      {formatPrice(productList.price)}
+                    </span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
   )
