da54d08d61ed14a377de1b1c3521d41c231cb1d9
luizmantuano <luguto23@hotmail.com>
2024-06-11 12:55:48 -0300

    feat:

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,76 +8,72 @@ import { FaRegHandshake } from 'react-icons/fa6'
 export default function About() {
   return (
     <div className="flex flex-col mt-[70px] md:flex-row" id="about">
-      <div className="mt-[50px] md:flex md:w-full  ">
+      <div className="mt-[50px] md:flex md:w-full">
         <img src={Img3} alt="#" className="object-contain w-[600px]" />
       </div>
-      <div className="space-y-7 md:grid md:grid-cols-2 md:w-full">
-        <div className="flex flex-col items-center justify-center md:absolute md:mt-[-70px]">
+      <div className="space-y-7 md:w-full md:flex md:flex-col ">
+        <div className="flex flex-col items-center justify-center md:mt-[-70px] md:mr-[80px] mb-[90px]">
           <h1
             className="text-[32px] font-bold m
-          l-[20px] mt-[20px] text-center z-"
+          l-[20px] mt-[80px] text-center "
           >
             Sobre nós
           </h1>
           <img src={Linha} alt="Line" />
         </div>
-        <div className="ml-[20px] flex flex-col items-center space-x-6">
-          <div className="flex flex-row w-full items-center ml-10 mb-2">
-            <Users size={60} color="#CC0D03" />
-            <h1 className="text-[16px] font-bold ml-5">Quem Somos</h1>
+        <div className="md:grid md:grid-cols-2 md:gap-20 md:mr-[100px]">
+          <div className="ml-[20px] flex flex-col space-x-6 mb-6">
+            <div className="flex flex-row  items-center ml-10 mb-2">
+              <Users size={60} color="#CC0D03" />
+              <h1 className="text-[16px] font-bold ml-5">Quem Somos</h1>
+            </div>
+            <div>
+              <p className="text-gray-400 ml-[25px] min-w-[250px]">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Officia explicabo aliquid voluptas quod quos, eaque.
+              </p>
+            </div>
           </div>
-          <div>
-            <p className="text-gray-400 ">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
-              explicabo aliquid voluptas quod quos, eaque, itaque ab fugit hic
-              sapiente dolorem ipsam consectetur recusandae! Animi praesentium
-              mollitia odit qui ut?
-            </p>
+          <div className="ml-[20px] flex flex-col space-x-6 mb-6">
+            <div className="flex flex-row  items-center ml-10 mb-2">
+              <TbTargetArrow size={60} color="#CC0D03" />
+              <h1 className="text-[16px] font-bold ml-5">Nossa Missão</h1>
+            </div>
+            <div>
+              <p className="text-gray-400 ml-[25px] min-w-[250px]">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Officia explicabo aliquid voluptas quod quos, eaque.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="ml-[20px] flex flex-col items-center space-x-6">
-          <div className="flex flex-row w-full items-center ml-10 mb-2">
-            <TbTargetArrow size={60} color="#CC0D03" />
-            <h1 className="text-[16px] font-bold ml-5">Nossa Missão</h1>
-          </div>
-          <div>
-            <p className="text-gray-400">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
-              explicabo aliquid voluptas quod quos, eaque, itaque ab fugit hic
-              sapiente dolorem ipsam consectetur recusandae! Animi praesentium
-              mollitia odit qui ut?
-            </p>
-          </div>
-        </div>
 
-        <div className="ml-[20px] flex flex-col items-center space-x-6">
-          <div className="flex flex-row w-full items-center ml-10 mb-2">
-            <TbCheckupList size={60} color="#CC0D03" />
-            <h1 className="text-[16px] font-bold ml-5">Produtos e Serviços</h1>
+          <div className="ml-[20px] flex flex-col space-x-6 mb-6">
+            <div className="flex flex-row items-center ml-10 mb-2">
+              <TbCheckupList size={60} color="#CC0D03" />
+              <h1 className="text-[16px] font-bold ml-5">
+                Produtos e Serviços
+              </h1>
+            </div>
+            <div>
+              <p className="text-gray-400 ml-[25px] min-w-[250px]">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Officia explicabo aliquid voluptas quod quos, eaque.
+              </p>
+            </div>
           </div>
-          <div>
-            <p className="text-gray-400">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
-              explicabo aliquid voluptas quod quos, eaque, itaque ab fugit hic
-              sapiente dolorem ipsam consectetur recusandae! Animi praesentium
-              mollitia odit qui ut?
-            </p>
-          </div>
-        </div>
-        <div className="ml-[20px] flex flex-col items-center space-x-6">
-          <div className="flex flex-row w-full items-center ml-10 mb-2">
-            <FaRegHandshake size={60} color="#CC0D03" />
-            <h1 className="text-[16px] font-bold ml-5">
-              Compromisso com o Cliente
-            </h1>
-          </div>
-          <div>
-            <p className="text-gray-400">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
-              explicabo aliquid voluptas quod quos, eaque, itaque ab fugit hic
-              sapiente dolorem ipsam consectetur recusandae! Animi praesentium
-              mollitia odit qui ut?
-            </p>
+          <div className="ml-[20px] flex flex-col space-x-6 mb-6">
+            <div className="flex flex-row items-center ml-10 mb-2">
+              <FaRegHandshake size={60} color="#CC0D03" />
+              <h1 className="text-[16px] font-bold ml-5">
+                Compromisso com o Cliente
+              </h1>
+            </div>
+            <div>
+              <p className="text-gray-400 ml-[25px] min-w-[250px]">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Officia explicabo aliquid voluptas quod quos, eaque.
+              </p>
+            </div>
           </div>
         </div>
       </div>
